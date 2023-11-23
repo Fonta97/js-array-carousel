e341f715b9868c18c6855aec9a461000d1b0539a
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-23 19:00:16 +0100

    fix: carousel img feat: js working next, implemented previous

--- js/script.js
+++ js/script.js
@@ -7,14 +7,39 @@ const next = document.querySelector('.next');
 next.addEventListener('click', function () {
 
     if (activeItem < items.length - 1) {
+
         items[activeItem].classList.remove('active');
+        items[activeItem].classList.add('hidden');
 
         activeItem++;
 
         items[activeItem].classList.add('active');
+        items[activeItem].classList.remove('hidden');
+
 
         if (activeItem === items.length - 1) {
             next.classList.add('hidden');
+        }
+    }
+});
+
+const previous = document.querySelector('.previous')
+
+previous.addEventListener('click', function () {
+
+    if (activeItem < items.length + 1) {
+
+        items[activeItem].classList.remove('active');
+        items[activeItem].classList.add('hidden');
+
+        activeItem--;
+
+        items[activeItem].classList.add('active');
+        items[activeItem].classList.remove('hidden');
+
+
+        if (activeItem === items.length + 1) {
+            previous.classList.add('hidden');
         }
     }
 });
